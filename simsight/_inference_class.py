db9971a5213e193c55eafa13ab4ebe8b8cb2a14c
rhoxu <roxburghhugh@gmail.com>
2026-07-16 15:26:05 +0800

    bruh

--- simsight/_inference_class.py
+++ simsight/_inference_class.py
@@ -459,7 +459,14 @@ class Inference:
                         np.ones_like(subsightline.sub_Grid).astype(bool),
                         t_grid, 0
                     )
-                    unit_density_j = resampled[mask]
+                    density_true_j = resampled[mask]
+
+                    omega_b = self.sim.cosmo.Ob0
+                    omega_m = self.sim.cosmo.Om0
+                    m_gas_true_h = halo['GasMass']   # <-- whatever field holds true integrated gas mass
+                    scale_h = (omega_b / omega_m) * halo['TotalMass'] / m_gas_true_h
+
+                    unit_density_j = density_true_j * scale_h
 
                 # still take elementwise max ACROSS OVERLAPPING HALOS
                 # (this max is fine -- it's halo-vs-halo, not halo-vs-IGM,
