d42634e6b815893ad930dcb1d7d0afbcac7249b5
rhoxu <roxburghhugh@gmail.com>
2026-07-31 11:37:38 +0800

    bruh

--- simsight/simsight.py
+++ simsight/simsight.py
@@ -817,9 +817,10 @@ class SightlineSim():
             os.environ['OPENBLAS_NUM_THREADS'] = '1'
             os.environ['MKL_NUM_THREADS'] = '1'
 
-            sightlines = Parallel(n_jobs=self.num_cores, backend='loky')(
+            new_sightlines = Parallel(n_jobs=self.num_cores, backend='loky')(
                 delayed(sl.model_sightline)(inference,filters,verbose=False,reduce=reduce) for sl in _Smart_Tqdm(sightlines,desc='Modelling sightlines')
                 )
+            sightlines = new_sightlines
         else:
             for sl in _Smart_Tqdm(sightlines, desc='Modelling sightlines'):
                 sl.model_sightline(inference,filters,verbose=False,reduce=reduce)
